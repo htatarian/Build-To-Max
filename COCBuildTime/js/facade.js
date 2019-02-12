5424b161a9ec5181855f14efc2653c034a84eb4f
Harout Tatarian <harouttatarian@hotmail.com>
2019-02-12 13:05:14 -0500

    Added COC API Call

--- COCBuildTime/js/facade.js
+++ COCBuildTime/js/facade.js
@@ -1,12 +1,14 @@
 function showResults() {
+    $("#txtError").html("");
     var gamerTag = $("#txtGamerTag").val();
 //    1. Offline validation
     if (doOfflineValidation(gamerTag)) {
         console.info("valid.");
     } else {
-        console.info("not valid.");
+        $("#txtError").html("Gamer Tag must be 8 alphanumeric characters.");
     }
 //    2. Server validation
+
 //    3. Fetch results
 //    4. Calculate
 //    5. Display
