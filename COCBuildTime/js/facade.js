bae1aba931a13f3d18dc638eaaa006455d385756
Harout Tatarian <harouttatarian@hotmail.com>
2019-02-12 18:06:38 -0500

    Fetching from sever
    API works now. King time is calculated correctly

--- COCBuildTime/js/facade.js
+++ COCBuildTime/js/facade.js
@@ -1,19 +1,38 @@
 function showResults() {
     $("#txtError").html("");
-    var gamerTag = $("#txtGamerTag").val();
-//    1. Offline validation
-    if (doOfflineValidation(gamerTag)) {
-        console.info("valid.");
-    } else {
-        $("#txtError").html("Gamer Tag must be 8 alphanumeric characters.");
-    }
-//    2. Server validation
-
-//    3. Fetch results
-//    4. Calculate
-//    5. Display
+    const gamerTag = $("#txtGamerTag").val();
+    //    1. Server validation
+    $.ajax( {
+        url: "PHP/getPlayerInfo.php",
+        data: {gamerTag:gamerTag},
+        success: EvaluateResult,
+        error: OnServerFail
+    });
 }
 
+function EvaluateResult(data) {
+    $("#txtError").html("")
+    var info = JSON.parse(data);
+
+    if(info.name == null)
+    {
+        $("#txtError").html("Not a valid info tag.");
+    }
+    else
+    {
+        $("#playerName").val(info.name);
+        $("#txtTownHallLevel").val(info.townHallLevel);
+        $("#txtKingLevel").val(getKingLevel(info));
+        $("#txtQueenLevel").val(getQueenLevel(info));
+        $("#txtKingTime").val(calculateKingTime(info));
+        // $("#txtQueenTime").val(calculateQueenTime(info));
+        // $("#txtSpellTime").val(calculateSpellTime(info));
+    }
+}
+
+function OnServerFail(){
+    $("#txtError").html("Something went wrong. Try again later.");
+}
 
 
 
--- COCBuildTime/js/util.js
+++ COCBuildTime/js/util.js
@@ -1,4 +1,105 @@
-function doOfflineValidation(gamerTag) {
-    const regex = /(^[a-zA-Z0-9]{8}$)/;
-    return regex.test(gamerTag);
+function calculateKingTime(info) {
+    var townHallLevel = info.townHallLevel;
+    var kingLevel = getKingLevel(info);
+    alert(kingLevel);
+    var kingChart = [
+        [7, 0],
+        [7, 12],
+        [7, 12],
+        [7, 24],
+        [7, 24],
+        [8, 24],
+        [8, 36],
+        [8, 36],
+        [8, 36],
+        [8, 48],
+        [9, 48],
+        [9, 48],
+        [9, 60],
+        [9, 60],
+        [9, 60],
+        [9, 72],
+        [9, 72],
+        [9, 72],
+        [9, 84],
+        [9, 84],
+        [9, 84],
+        [9, 96],
+        [9, 96],
+        [9, 96],
+        [9, 108],
+        [9, 108],
+        [9, 108],
+        [9, 120],
+        [9, 120],
+        [9, 120],
+        [10, 132],
+        [10, 132],
+        [10, 132],
+        [10, 144],
+        [10, 144],
+        [10, 144],
+        [10, 156],
+        [10, 156],
+        [10, 156],
+        [10, 168],
+        [11, 168],
+        [11, 168],
+        [11, 168],
+        [11, 168],
+        [11, 168],
+        [11, 168],
+        [11, 168],
+        [11, 168],
+        [11, 168],
+        [11, 168],
+        [12, 168],
+        [12, 168],
+        [12, 168],
+        [12, 168],
+        [12, 168],
+        [12, 168],
+        [12, 168],
+        [12, 168],
+        [12, 168],
+        [12, 168],
+    ];
+
+    var totalTime = 0;
+
+    for(var i = kingLevel; i < kingChart.length && townHallLevel >= kingChart[i][0]; i++)
+    {
+        alert(i+ ": " + kingChart[i][0] + " " + kingChart[i][1]);
+        totalTime = totalTime + kingChart[i][1];
+    }
+
+    return totalTime;
+}
+
+function getKingLevel(info) {
+    var heroes = info.heroes;
+    var kingLevel = 1;
+
+    if (heroes !== null) {
+        for (var i = 0; i < heroes.length; i++) {
+            if (heroes[i].name === "Barbarian King") {
+                kingLevel = heroes[i].level;
+            }
+        }
+    }
+    return kingLevel;
+}
+
+function getQueenLevel(info) {
+    var heroes = info.heroes;
+    var queenLevel = 1;
+
+    if (heroes !== null) {
+        for (var i = 0; i < heroes.length; i++) {
+            if (heroes[i].name === "Archer Queen") {
+                queenLevel = heroes[i].level;
+            }
+        }
+    }
+    return queenLevel;
 }
